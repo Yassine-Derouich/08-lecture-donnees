863687370573e7b68a802bcadeef7a41915ae2d8
Yassine Derouich <yassine.derouich@edu.esiee.fr>
2025-11-09 14:42:23 +0000

    119 tests validés

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ FILENAME = "listes.csv"
 
 #### Fonctions secondaires
 
-def read_data(filename):
+def read_data(FILENAME):
     """retourne le contenu du fichier <filename>
 
     Args:
@@ -14,38 +14,54 @@ def read_data(filename):
         list: le contenu du fichier (1 list par ligne)
     """
     l = []
+    
+    with open(FILENAME,"r") as f:
+        for line in f:
+            cleandata = line.replace("\n","").split(";")
+            for index, value in enumerate(cleandata):
+                cleandata[index] = int(value)
+            l.append(cleandata)
     return l
 
 def get_list_k(data, k):
-    l = []
-    return l
-
+    return data[k]
+    
 def get_first(l):
-    return None
+    return l[0]
 
 def get_last(l):
-    return None
+    return l[len(l)-1]
 
 def get_max(l):
-    return None
+    maximum = 0
+    for i in l:
+        if i > maximum:
+            maximum = i
+    return maximum
 
 def get_min(l):
-    return None
-
+    minimum = get_max(l)
+    for i in l:
+        if  i < minimum:
+            minimum = i
+    return minimum
+    
 def get_sum(l):
-    return None
-
+    total = 0
+    for i in l:
+        total += int(i)
+    return total
 
 #### Fonction principale
 
 
 def main():
     pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
+    data = read_data(FILENAME)
+    for i, l in enumerate(data):
+        print(i, l)
+    k = 37
+    print(k, get_max(data))
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,8 +1,34 @@
+filename = "listes.csv"
+smaliste = ['90', '41', '34', '80', '84', '89', '65', '67', '63', '64']
 tab = []
 tab2 = []
-with open("listes.csv","r") as f:
-    while f.readline():
-        cleandata = f.readline().replace("\n","")
+
+with open(filename,"r") as f:
+    for line in f:
+        cleandata = line.replace("\n","")
         tab2 = [cleandata]
         tab.append(tab2)
 
+def read_data(filename):
+    """retourne le contenu du fichier <filename>
+
+    Args:
+        filename (str): nom du fichier à lire
+
+    Returns:
+        list: le contenu du fichier (1 list par ligne)
+    """
+    l = []
+    
+    with open("listes.csv","r") as f:
+        for line in f:
+            cleandata = line.replace("\n","").split(";")
+            for index, value in enumerate(cleandata):
+                cleandata[index] = int(value)
+            l.append(cleandata)
+    return l
+
+# OK - print(read_data(filename))
+laliste = read_data(filename)
+
+print(laliste)
